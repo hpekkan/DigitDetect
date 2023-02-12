81f94f31a02a2227bf08a1c150861b363a049d3f
hpekkan <hpekkan@etu.edu.tr>
2023-02-12 13:50:35 +0300

    paint app implemantation

---
+++ src/components/Menu.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "../App.css";
+  
+const Menu = ({ setLineColor, setLineWidth, 
+setLineOpacity }) => {
+  return (
+    <div className="Menu">
+      <label>Brush Color </label>
+      <input
+        type="color"
+        onChange={(e) => {
+          setLineColor(e.target.value);
+        }}
+      />
+      <label>Brush Width </label>
+      <input
+        type="range"
+        min="3"
+        max="20"
+        onChange={(e) => {
+          setLineWidth(e.target.value);
+        }}
+      />
+      <label>Brush Opacity</label>
+      <input
+        type="range"
+        min="1"
+        max="100"
+        onChange={(e) => {
+          setLineOpacity(e.target.value / 100);
+        }}
+      />
+    </div>
+  );
+};
+  
+export default Menu;
